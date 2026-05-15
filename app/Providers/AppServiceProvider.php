a31724d3142855b1fad860bd606effe705994617
Will Regelmann <will.regelmann@commandlink.com>
2026-05-15 12:26:34 -0400

    Guard default user creation behind a schema existence check
    Prevents a fatal error during migrations when the users table doesn't exist yet, since AppServiceProvider boots before the schema is in place on a fresh install.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,9 +15,11 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        User::firstOrCreate(
-            ['email' => 'local@localhost'],
-            ['name' => 'Local User', 'password' => '']
-        );
+        if (Schema::hasTable('users')) {
+            User::firstOrCreate(
+                ['email' => 'local@localhost'],
+                ['name' => 'Local User', 'password' => '']
+            );
+        }
     }
 }
